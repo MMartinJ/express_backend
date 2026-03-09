3c0f364ed4990ecbb1c893e366f9140dcaa40d16
MMartinJ <marina.m.jurado86@gmail.com>
2026-03-09 19:54:25 +0100

    first step without db to deliver data to the client

--- server.js
+++ server.js
@@ -1,11 +1,54 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('¡Hola! Mi servidor Express está vivo en Portainer 🚀');
+
+const products = [
+  {
+    id: 1,
+    imageSrc: "images/iphone.png",
+    title: "iPhone 15 Pro",
+    specification: [
+      "A17 Pro chip with 6-core GPU",
+      "3x or 5x Telephoto camera",
+      "Up to 29 hours video playback"
+    ],
+    stockCount: 10,
+    price: 999,
+  },
+  {
+    id: 2,
+    imageSrc: "images/airpods.png",
+    title: "AirPods Pro 2",
+    specification: [
+      "Noise Cancellation",
+      "Dust, sweat, and water resistant",
+      "Up to 6 hours of listening",
+    ],
+    stockCount: 0,
+    price: 200,
+  },
+  {
+    id: 3,
+    imageSrc: "images/apple-watch.png",
+    title: "Apple Watch 9",
+    specification: [
+      "45mm or 41mm case size",
+      "Always-On Retina display",
+      "Up to 18 hours normal use",
+    ],
+    stockCount: 6,
+    price: 400,
+  }
+
+
+];
+
+
+app.use(cors())
+app.get('/api/products',(req,res) => {
+    res.json(products);
 });
 
-app.listen(port, () => {
-  console.log(`API escuchando en el puerto ${port}`);
-});
+
+app.listen(5000);
